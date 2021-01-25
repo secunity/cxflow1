2fab542f164420e81fbef962387b728aad0746ce
YairN <yair@secunity.io>
2021-01-25 08:43:03 +0000

    logfile env variables parsing

--- conf.py
+++ conf.py
@@ -22,10 +22,10 @@ def get_args_parser():
     parser.add_argument('-c', '--config', type=str, help='Config file (overriding all other options)', default=None)
 
     parser.add_argument('-l', '--logfile', type=str, help='File to log to', default=None)
-    parser.add_argument('-v', '--verbose', type=bool, help='Indicates whether to log verbose data', default=False)
+    parser.add_argument('-v', '--verbose', type=bool, help='Indicates whether to log verbose data', default=None)
 
-    parser.add_argument('--to_stdout', '--stdout', type=str, help='Log messages to stdout', default=False)
-    parser.add_argument('--to_stderr', '--stderr', type=str, help='Log errors to stderr', default=False)
+    parser.add_argument('--to_stdout', '--stdout', type=str, help='Log messages to stdout', default=None)
+    parser.add_argument('--to_stderr', '--stderr', type=str, help='Log errors to stderr', default=None)
 
     parser.add_argument('-p', '--port', type=str, help='Listening port (UDP)', default=None)
     parser.add_argument('--identifier', '--id', type=str, help='Agent ID - must be XFlow Agent ID', default=None)
